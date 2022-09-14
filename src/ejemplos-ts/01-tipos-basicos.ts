ab0e96fab2aa20b998c9c83ba6117b5be2de6771
EduardoCardenasBarajas <jecarbar96@gmail.com>
2022-09-13 20:47:13 -0500

    cuarto commit

--- src/ejemplos-ts/01-tipos-basicos.ts
+++ src/ejemplos-ts/01-tipos-basicos.ts
@@ -1,8 +1,23 @@
 
 
-console.log('Hola Mundo!-IEVN-1001');
+//variables tipos de datos
+var num1=2;
+let num2=3;
+const num3=4;
 
-/*
-    ===== Código de TypeScript =====
-*/
+
+//function suma(){
+//    const tem = 3;
+//    console.log(tem);
+//    tem=99;
+//}
+
+//suma();
+let nombre:string='Eduardo';
+let num:number=23;
+let activo:boolean=true;
+
+let matricula:number | string |boolean ="266jshs";
+matricula=8273263;
+matricula=false;
 
